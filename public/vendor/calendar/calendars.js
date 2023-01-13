d4c3b3bd24eed0a62d2e39aed6adda9268fc0a6b
masif088 <mokhamadasif@gmail.com>
2023-01-13 07:15:21 +0700

    update README.md add simple calendar

---
+++ public/vendor/calendar/calendars.js
@@ -0,0 +1,43 @@
+'use strict';
+
+/* eslint-disable require-jsdoc, no-unused-vars */
+
+var CalendarList = [];
+
+function CalendarInfo() {
+    this.id = null;
+    this.name = null;
+    this.checked = true;
+    this.color = null;
+    this.bgColor = null;
+    this.borderColor = null;
+    this.dragBgColor = null;
+}
+
+function addCalendar(calendar) {
+    CalendarList.push(calendar);
+}
+
+function findCalendar(id) {
+    var found;
+
+    CalendarList.forEach(function(calendar) {
+        if (calendar.id === id) {
+            found = calendar;
+        }
+    });
+
+    return found || CalendarList[0];
+}
+
+function hexToRGBA(hex) {
+    var radix = 16;
+    var r = parseInt(hex.slice(1, 3), radix),
+        g = parseInt(hex.slice(3, 5), radix),
+        b = parseInt(hex.slice(5, 7), radix),
+        a = parseInt(hex.slice(7, 9), radix) / 255 || 1;
+    var rgba = 'rgba(' + r + ', ' + g + ', ' + b + ', ' + a + ')';
+
+    return rgba;
+}
+
